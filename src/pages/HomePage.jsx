237f6b64813adf6d2561ca30f2388ee73b77cf68
ibrahim ethem ispir <ethem6046@gmail.com>
2024-04-27 16:41:23 +0300

    mantine ui paketleri eklendi

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import { TextInput, Checkbox, Button, Group, Box } from "@mantine/core";
+import { useForm } from "@mantine/form";
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: {
+      email: "",
+      termsOfService: false,
+    },
 
-export default HomePage
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+    },
+  });
+
+  return (
+    <Box maw={340} mx="auto">
+      <form onSubmit={form.onSubmit((values) => console.log(values))}>
+        <TextInput
+          withAsterisk
+          label="Email"
+          placeholder="your@email.com"
+          key={form.key("email")}
+          {...form.getInputProps("email")}
+        />
+
+        <Checkbox
+          mt="md"
+          label="I agree to sell my privacy"
+          key={form.key("termsOfService")}
+          {...form.getInputProps("termsOfService", { type: "checkbox" })}
+        />
+
+        <Group justify="flex-end" mt="md">
+          <Button type="submit">Submit</Button>
+        </Group>
+      </form>
+    </Box>
+  );
+};
+
+export default HomePage;
